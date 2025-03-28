453d58434c83160c789c641d4d5f04d6743eb495
Gabriel Cabrera <gcabrera@posibillian.tech>
2025-03-28 18:11:51 -0300

    redirect to home

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,23 +3,35 @@
 import { useState } from "react";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
 
     // Llamada al backend NestJS para autenticar usuario
-    const res = await fetch("http://localhost:3000/auth/login", {
+    const res = await fetch("http://localhost:4000/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username, password }),
     });
 
     if (res.ok) {
-      alert("Login exitoso");
-    } else {
-      alert("Credenciales incorrectas");
+       const data = await res.json();
+
+      if (data.status === "ok") {
+
+        if (typeof window !== "undefined") { // Verificamos que estamos en el cliente
+          window.location.href = "/"; // Redirige a la ruta raíz
+        }
+
+      }else {
+        alert(data.message);
+      }
+    }else{
+      const errorData = await res.json();
+      console.error("Error:", errorData);
+      alert("Error en la autenticación");
     }
   };
 
@@ -28,10 +40,10 @@ export default function LoginPage() {
       <form onSubmit={handleLogin} className="bg-white p-6 rounded-lg shadow-lg">
         <h1 className="text-2xl text-black font-bold mb-4">Iniciar Sesión</h1>
         <input
-          type="email"
+          type="text"
           placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
           className="w-full p-2 text-black border mb-3"
           required
         />
