0b5cbadd8c0554cf299920b169cb0873fd55efc6
Gabriel Cabrera <gcabrera@posibillian.tech>
2025-08-26 16:27:10 -0300

    tab ventas con delivery

--- src/app/facturacion/page.tsx
+++ src/app/facturacion/page.tsx
@@ -5,33 +5,38 @@ import Link from "next/link";
 import FacturacionView from "./components/facturacionView";
 import VentasView from "./components/ventasView";
 import InputModal from "../components/modalConInput";
+import VentasDeliveryView from "./components/ventasDeliveryView";
 
 
-
-const tabs = ["Facturación", "Ventas"];
+const tabs = ["Facturación", "Ventas", "Ventas con Delivery"];
 
 export default function StockTabsPage() {
   const [activeTab, setActiveTab] = useState("Facturación");
+  const [selectedTab, setSelectedTab] = useState("Facturación");
   const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleConfirm = (password: string) => {
       console.log("Contraseña ingresada:", password);
 
       if (password === "1234.") {
+      if(selectedTab==="Ventas"){
        setActiveTab("Ventas");
+
+      } else if (selectedTab === "Ventas con Delivery") {
+        setActiveTab("Ventas con Delivery");
+      }
       } else {
         alert("Contraseña incorrecta ❌");
       }
-
       setIsModalOpen(false);
     };
 
   const openModal = (tab: string) => {
-
+      setSelectedTab(tab);
     if (tab === "Facturación") {
        setActiveTab(tab);
     }
-    if (tab === "Ventas") {
+      else if (tab === "Ventas con Delivery" || tab === "Ventas") {
        setIsModalOpen(true);
     }
 
@@ -58,6 +63,7 @@ export default function StockTabsPage() {
       <div>
         {activeTab === "Facturación" && <FacturacionView />}
         {activeTab === "Ventas" && <VentasView />}
+        {activeTab === "Ventas con Delivery" && <VentasDeliveryView />}
 
       </div>
       </div>
